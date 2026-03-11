eb9ba6ff497d395abd1d93b7016af0fd2cb428ed
Sr <romeromorerraul@gmail.com>
2026-03-11 19:09:10 +0100

    Reapply "Almost full gameplay localization"
    This reverts commit 4294c2a40ee0d6b73a0380edb1d36b280ea5a2e4.

---
+++ Assets/Scripts/LocalizeTextProDescription.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class LocalizeTextProDescription : MonoBehaviour
+{
+    public InventoryUI playerInventory;
+    private TextMeshPro textValue;
+
+    private string descriptionKey;
+
+    private ItemSlotUI selectedSlot;
+
+    void Start()
+    {
+        textValue = GetComponent<TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        selectedSlot = playerInventory.GetSelectedItemSlotUI();
+
+        if (selectedSlot)
+        {
+            ItemBase selectedItem = selectedSlot.GetItem();
+            descriptionKey = selectedItem.descriptionTextKey;
+
+            textValue.text = GameplayLocalizer.GetText(descriptionKey);
+        }
+    }
+}
